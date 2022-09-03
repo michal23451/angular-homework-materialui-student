d526245590821f7148c27ea3a1e1ed0266727b75
Michał Konowalski <konowalski.michal@gmail.com>
2022-09-03 22:57:23 +0200

    creating html and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { StudentHomeComponent } from './student-home/student-home.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+
+const routes: Routes = [
+  {path:"", redirectTo: "home", pathMatch: "full"},
+  {path:"home", component: StudentHomeComponent},
+  {path:"students", component: StudentListComponent},
+  {path:"add-student-form", component: StudentFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentHomeComponent } from './student-home/student-home.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentServiceComponent } from './student-service/student-service.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentHomeComponent,
+    StudentListComponent,
+    StudentFormComponent,
+    StudentServiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
